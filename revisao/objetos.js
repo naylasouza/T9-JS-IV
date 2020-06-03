09a408384f8e8fec2ce71f5b5ba26c96afe3cf6f
nayla souza ribeiro <naylasouzariibeiro@gmail.com>
2020-06-03 09:50:55 -0300

    minha resolucao

---
+++ revisao/objetos.js
@@ -0,0 +1,17 @@
+const pessoa = {
+    personalidade: ['personalidade1', 'personalidade2'];
+    areaAtuacao: 'biologa'
+}
+
+console.log(typeof(pessoa))
+
+const cliente1 = {
+    nome: "dora barreto",
+    cpf: '223.001.221-29',
+    valorCompra: 2000,
+    modeloCelular:'XXX-12',
+    anoCelular: 2020,
+    CorCelular: 'amarelo',
+    Loja: "rj-br",
+    atendente: 'joana maria',
+}
