cdfdb7727abcf4fbbc529f3c10d791f0965e3cca
nayla souza ribeiro <naylasouzariibeiro@gmail.com>
2020-06-04 09:40:32 -0300

    exercicio quarta MEU

---
+++ revisao/exercicios_objeto/analise_objetos.js
@@ -0,0 +1,28 @@
+//Dado o objeto, analise a forma que foi declarada. 
+//Em seguida, ajuste o objeto produtos da forma correta de se declarar.
+// let produtos = {
+//   videos: {
+//     comedia: ["comedia1", "comedia2"],
+//     romance: ["romance1", "romance2"],
+//   },
+//   revistas: {
+//     moda: ["lalala", "lililili"],
+//     saude: ["lalala2", "lililili2"],
+//   },
+//   jogos: {
+//     rpg: ["rpg1", "rpg2", 'rpg3'],
+//     acao: ["acao1", "God of War"],
+//   },
+// }
+// console.log(produtos)
+
+class Signos {
+  constructor(nome, elemento, regente) {
+    this.nome = nome;
+    this.elemento = elemento;
+    this.regente = regente;
+  }
+
+  }
+  const pessoa1 = new Signos('capricornio', 'terra', 'regente')
+  console.log(pessoa1)
